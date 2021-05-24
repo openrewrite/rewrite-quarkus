1686f2ace17a5e0fc48343e0a2f41037bbfa2494
Patrick Way <pway99@users.noreply.github.com>
2021-05-24 12:14:07 -0700

    Mutiny api update (#11)
    * Quarkus Mutiny API updates for `Multi.collectItems`, `Multi.groupItems`, `Multi.transform` and `MultiTransform.byTakingFirstItems`. Fixes #4

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ![Logo](https://github.com/openrewrite/rewrite/raw/main/doc/logo-oss.png)
 ### Apply Quarkus upgrades and best practices automatically
+
 [![Apache 2.0](https://img.shields.io/github/license/openrewrite/rewrite-testing-frameworks.svg)](https://www.apache.org/licenses/LICENSE-2.0)
 [![Maven Central](https://img.shields.io/maven-central/v/org.openrewrite.recipe/rewrite-quarkus.svg)](https://mvnrepository.com/artifact/org.openrewrite.recipe/rewrite-quarkus)
+[![ci](https://github.com/openrewrite/rewrite-quarkus/actions/workflows/ci.yml/badge.svg)](https://github.com/openrewrite/rewrite-quarkus/actions/workflows/ci.yml)
+
 
 ### What is this?
 
---
+++ src/test/kotlin/org/openrewrite/java/quarkus/QuarkusUpgradeIntegrationTest.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.quarkus
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.Recipe
+import org.openrewrite.config.Environment
+import org.openrewrite.java.JavaParser
+import org.openrewrite.java.JavaRecipeTest
+
+class QuarkusUpgradeIntegrationTest : JavaRecipeTest {
+    override val parser: JavaParser = JavaParser.fromJavaVersion()
+        .logCompilationWarningsAndErrors(true)
+        .classpath("mutiny", "reactive-streams")
+        .build()
+
+    override val recipe: Recipe = Environment.builder()
+        .scanRuntimeClasspath("org.openrewrite.java.quarkus")
+        .build()
+        .activateRecipes(
+            "org.openrewrite.java.quarkus.Quarkus1to1_13Migration"
+        )
+
+    @Test
+    fun changeMultiTransformAndByTakingFirst() = assertChanged(
+        before = """
+            import io.smallrye.mutiny.Multi;
+            import io.smallrye.mutiny.Uni;
+            import java.util.List;
+            import java.time.Duration;
+            public class A {
+                public Multi<String> greetings(int count, String name) {
+                    return Multi.createFrom().ticks().every(Duration.ofMillis(1))
+                            .onItem().transform(n -> "hello " + name + " -" + n)
+                            .transform().byTakingFirstItems(count);
+                }
+                public Uni<List<String>> collectItems(int count, String name) {
+                    Multi<String> multi = greetings(count, name);
+                    Uni<List<String>> uni = multi.collectItems().asList();
+                    return uni;
+                }
+            }
+        """,
+        after = """
+            import io.smallrye.mutiny.Multi;
+            import io.smallrye.mutiny.Uni;
+            import java.util.List;
+            import java.time.Duration;
+            public class A {
+                public Multi<String> greetings(int count, String name) {
+                    return Multi.createFrom().ticks().every(Duration.ofMillis(1))
+                            .onItem().transform(n -> "hello " + name + " -" + n)
+                            .select().first(count);
+                }
+                public Uni<List<String>> collectItems(int count, String name) {
+                    Multi<String> multi = greetings(count, name);
+                    Uni<List<String>> uni = multi.collect().asList();
+                    return uni;
+                }
+            }
+        """
+    )
+}
