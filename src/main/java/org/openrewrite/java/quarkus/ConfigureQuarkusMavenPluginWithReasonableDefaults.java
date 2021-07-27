3891c9a284f997b2673e959a87e29dc47b2186d4
Jonathan Schneider <jkschneider@gmail.com>
2021-07-26 17:26:01 -0700

    refactor: Automate fixing Sonar issues. (#51)
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/openrewrite/java/quarkus/ConfigureQuarkusMavenPluginWithReasonableDefaults.java
+++ src/main/java/org/openrewrite/java/quarkus/ConfigureQuarkusMavenPluginWithReasonableDefaults.java
@@ -69,4 +69,3 @@ public class ConfigureQuarkusMavenPluginWithReasonableDefaults extends Recipe {
         }
     }
 }
-
--- src/main/java/org/openrewrite/java/quarkus/MigrateQuarkusMavenPluginNativeImageGoal.java
+++ src/main/java/org/openrewrite/java/quarkus/MigrateQuarkusMavenPluginNativeImageGoal.java
@@ -101,4 +101,3 @@ public class MigrateQuarkusMavenPluginNativeImageGoal extends Recipe {
     }
 
 }
-
